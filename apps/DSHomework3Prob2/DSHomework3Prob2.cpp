e66f349e359487e3f732cf12e54d142764b223c0
Braden Owens <bowens2@my.athens.edu>
2022-06-13 00:20:05 -0500

    addition of all sequential containers

--- apps/DSHomework3Prob2/DSHomework3Prob2.cpp
+++ apps/DSHomework3Prob2/DSHomework3Prob2.cpp
@@ -1,4 +1,6 @@
 // Homework 3 Problem 1 Data Structures
 // Braden Owens
 // 6/12/2022
-// 
+// there wasnt a CPP That I could submit with this problems project, but I
+// Have attached all of the sequential containers that I changed to support the iterator class to this project
+// along with the iterator class itself
---
+++ apps/DSHomework3Prob2/Iterator.h
@@ -0,0 +1,7 @@
+//Iterator class
+template <class C>
+class Iterator 
+{
+public:
+	virtual C * next() = 0;
+}
