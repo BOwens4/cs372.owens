eda5e08a112364ebd1231f9900571f4839784af2
Braden Owens <bowens2@my.athens.edu>
2022-06-24 18:59:16 -0500

    edit

--- apps/DSHomework4TowersofHanoi/DSHomework4TowersofHanoi.cpp
+++ apps/DSHomework4TowersofHanoi/DSHomework4TowersofHanoi.cpp
@@ -5,9 +5,62 @@
 
 #include <iostream>
 #include <chrono>
+
 using namespace std;
 void moveDisksrecur(int, int, int, int);
-void moveDisksiter(int, int, int, int);
+//void moveDisksiter(int, int, int, int);
+struct Stack
+{
+	int top;
+	int* arr;
+	unsigned cap;
+};
+
+
+struct Stack* create(unsigned cap)
+{
+	struct Stack* stack =
+		(struct Stack*)malloc(sizeof(struct Stack));
+	stack->cap = cap;
+	stack->top = -1;
+	stack->arr =
+		(int*)malloc(stack_ > cap * sizeof(int));
+	return stack;
+}
+void moveDisksBetweenTwoPoles(struct Stack* src, struct Stack* dest, char s, char d)
+{
+	int p1topdisk = pop(src);
+	int p2topdisk = pop(dest);
+
+	if (p1topdisk == int_min)
+	{
+		push(src, p2topdisk)
+	}
+}
+int empty(struct Stack* stack)
+{
+	return(stack->top == -1);
+}
+int pop(struct Stack* stack)
+{
+	if (empty(stack))
+		return INT_MIN;
+	return stack->arr[stack->top--];
+}
+void push(struct Stack* stack, int item)
+{
+	if (isFull(stack))
+		return;
+	stack-> arr[++stack->top] = item;
+}
+int full(struct Stack* stack)
+{
+	return (stack->top == stack->cap-1)
+}
+void moveDisk(char fromPeg, char toPeg, int disk)
+{
+	cout << "Move disk" << disk << "from" << fromPeg << "to" << toPeg << endl;
+}
 int main()
 {
 	const int NUMDISKS = 5;
@@ -30,4 +83,4 @@ void moveDisksrecur(int num, int fromPeg, int toPeg, int tempPeg)
 		moveDisksrecur(num - 1, tempPeg, toPeg, fromPeg);
 	}
 }
-void moveDisksiter
+//void moveDisksiter
