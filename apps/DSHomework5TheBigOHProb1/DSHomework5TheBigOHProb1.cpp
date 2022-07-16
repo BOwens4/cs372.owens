e2a0a0e18dc2156d504906a852c0cf76a163aef0
Braden Owens <bowens2@my.athens.edu>
2022-07-15 22:07:37 -0500

    final commit of hw, and guess for big oh

--- apps/DSHomework5TheBigOHProb1/DSHomework5TheBigOHProb1.cpp
+++ apps/DSHomework5TheBigOHProb1/DSHomework5TheBigOHProb1.cpp
@@ -2,6 +2,9 @@
 // Braden Owens
 // 7/7/2022
 // This program creates a function to implement the algorithm given
+// I try to run the program and my computer just freezes. it builds sucessfully but does not run.I can not get an output,even after letting my computer sit for a few minutes.
+//However I feel that most if not all of my logic is correct
+
 #include <iostream>
 #include <chrono>
 #include <list>
@@ -13,7 +16,7 @@ int anyEqual(int n, int **A)
         for (j = 0; j <= n; j++){
             for (k = 0; k <= n; k++){
                 for (m = 0; m <= n; m++){
-                    if (**A[i][j] == **A[k][m] && !(i == k && j == n))
+                    if (A[i][j] == A[k][m] && !(i == k && j == m))
                         return 1;
                 }
             }
@@ -22,18 +25,39 @@ int anyEqual(int n, int **A)
     return 0;
 }
 
-int main() 
+int main()
 {
+    int size = 0;
+    double temp = 0;
     list<int> sizes = { 10,20,30,40,50,100,500,1000 };
-    int average = 0;
+    double sum = 0;
+    double average = 0;
+    int** A;
+    A = (int**)malloc(size * sizeof(int*));
     auto start = chrono::steady_clock::now();
     auto end = chrono::steady_clock::now();
     for (auto size : sizes)
     {
         sum = 0;
-        for (int i = 0, i <= 20; i++)
+        for (int count = 0; count <= 20; count++)
         {
-            anyEqual(size, **A[][])
-                average = sum / 20
+            for (int row = 0; row < size; row++) {
+                A[row] = (int*)malloc(size * sizeof(char));
+            }
+            for (int row = 0; row < size; row++) {
+                for (int col = 0; col < size; col++) {
+                    A[row][col] = rand();
+                }
+            }
+            start = chrono::steady_clock::now();
+            anyEqual(size, A);
+            end = chrono::steady_clock::now();
+            chrono::duration<double>elapsed_seconds = end - start;
+            temp = elapsed_seconds.count();
+            sum += temp;
+            average = sum / 20;
+            cout << "Array of size: " << size << " had an average of: " << average << endl;
         }
     }
+    return 0;
+}
